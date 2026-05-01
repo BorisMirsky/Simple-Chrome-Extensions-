c3fb7552fadbe3f331a1796253b556b29b5a0550
Boris Mirsky <boris.y.mirsky@gmail.com>
2026-05-01 19:13:29 +0300

    first project ready

--- image_grabber/popup.js
+++ image_grabber/popup.js
@@ -1,9 +1,8 @@
 const grabBtn = document.getElementById("grabBtn");
-grabBtn.addEventListener("click",() => {    
-    // Получить активную вкладку браузера
+grabBtn.addEventListener("click",() => {
+    // Get active browser tab
     chrome.tabs.query({active: true}, function(tabs) {
         var tab = tabs[0];
-        // и если она есть, то выполнить на ней скрипт
         if (tab) {
             execScript(tab);
         } else {
@@ -13,13 +12,13 @@ grabBtn.addEventListener("click",() => {
 })
 
 /**
- * Выполняет функцию grabImages() на веб-странице указанной
- * вкладки и во всех ее фреймах,
- * @param tab {Tab} Объект вкладки браузера
+ * Execute a grabImages() function on a web page,
+ * opened on specified tab and on all frames of this page
+ * @param tab - A tab to execute script on
  */
 function execScript(tab) {
-    // Выполнить функцию на странице указанной вкладки
-    // и передать результат ее выполнения в функцию onResult
+    // Execute a function on a page of the current browser tab
+    // and process the result of execution
     chrome.scripting.executeScript(
         {
             target:{tabId: tab.id, allFrames: true},
@@ -30,42 +29,56 @@ function execScript(tab) {
 }
 
 /**
- * Получает список абсолютных путей всех картинок
- * на удаленной странице
- * 
- *  @return Array Массив URL
+ * Executed on a remote browser page to grab all images
+ * and return their URLs
+ *
+ *  @return Array of image URLs
  */
 function grabImages() {
     const images = document.querySelectorAll("img");
-    return Array.from(images).map(image=>image.src);    
+    return Array.from(images).map(image=>image.src);
 }
 
 /**
- * Выполняется после того как вызовы grabImages 
- * выполнены во всех фреймах удаленной web-страницы.
- * Функция объединяет результаты в строку и копирует  
- * список путей к изображениям в буфер обмена
- * 
- * @param {[]InjectionResult} frames Массив результатов
- * функции grabImages
+ * Executed after all grabImages() calls finished on
+ * remote page
+ * Combines results and copy a list of image URLs
+ * to clipboard
+ *
+ * @param {[]InjectionResult} frames Array
+ * of grabImage() function execution results
  */
 function onResult(frames) {
-    // Если результатов нет
-    if (!frames || !frames.length) { 
+    // If script execution failed on remote end
+    // and could not return results
+    if (!frames || !frames.length) {
         alert("Could not retrieve images from specified page");
         return;
     }
-    // Объединить списки URL из каждого фрейма в один массив
+    // Combine arrays of image URLs from
+    // each frame to a single array
     const imageUrls = frames.map(frame=>frame.result)
                             .reduce((r1,r2)=>r1.concat(r2));
-    // Скопировать в буфер обмена полученный массив  
-    // объединив его в строку, используя возврат каретки 
-    // как разделитель  
-    window.navigator.clipboard
-          .writeText(imageUrls.join("\n"))
-          .then(()=>{
-             // закрыть окно расширения после 
-             // завершения
-             window.close();
-          });
+    // Open a page with a list of images and send urls to it
+    openImagesPage(imageUrls)
+}
+
+/**
+ * Opens a page with list of URLs and UI to select and
+ * download them on a new browser tab and send an
+ * array of image URLs to this page
+ *
+ * @param {*} urls - Array of Image URLs to send
+ */
+function openImagesPage(urls) {
+    chrome.tabs.create(
+        {"url": "page.html",active:false},(tab) => {
+            // * Send `urls` array to this page
+            setTimeout(()=>{
+                chrome.tabs.sendMessage(tab.id,urls,(response) => {
+                    chrome.tabs.update(tab.id,{active: true});
+                });
+            },500);
+        }
+    );
 }
